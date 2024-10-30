a9f7740403d5d3aa81cc30411d5fe3ac1b93d008
banushasarma <sharmabanu03@gmail.com>
2024-10-30 20:44:56 +0530

    Bus Booking App init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { BookingComponent } from './pages/booking/booking.component';
+import { SearchComponent } from './pages/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full',
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+  {
+    path: 'booking',
+    component: BookingComponent,
+  },
+];
